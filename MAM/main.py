91b4ac5db30c1d26f7281c0627a5824e5bfa5154
marina <marina.andrade@sanofi.com>
2024-01-18 11:44:17 -0300

    including stat mape YTD and acov columns

--- MAM/main.py
+++ MAM/main.py
@@ -6,20 +6,32 @@ import os
 
 
 path = r'/Users/i0557167/Documents/Git/Statistical-Forecasting/MAM/data/' # use your path
-all_files = glob.glob(os.path.join(path, "*.csv"))
 
-df = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
-df_forecastable = df.loc[(df.is_forecastable == True)]
-df_coverage = df.loc[(df.is_forecastable == True) & (df.item_usage_rule == 'Use')]
-df_adherence = df_coverage.loc[df_coverage.baseline == '2. Statistical']
-df_not_adherence = df_coverage.loc[df_coverage.baseline == '1. Manual']
-
-
-number_items = len(df)
-number_forecastable = len(df_forecastable)
-number_coverage = len(df_coverage)
-adherence = len(df_adherence)
+agregated_kpi = pd.read_excel(path+'agregated_kpi.xlsx')
+monthly_kpi = pd.read_excel(path+'monthly_kpis.xlsx')
+forecastability = pd.read_csv(path+'SQL_Review_forecastability_2023_12_14.csv')
 
 
 
-df_test_new_items = df_forecastable.loc[(df_forecastable.item_usage_rule == 'Ignore') & (df_forecastable.intermit < 0.3)]
+kr_YTD  = agregated_kpi.loc[(agregated_kpi.MARKET == 'KOREA')].sort_values(by=['Sales with Stat Fcst'], ascending=False)
+
+kr_YTD.Volume = kr_YTD.Volume*100
+
+top10KR = kr_YTD.GMID.head(13)
+
+
+
+#df_forecastable = df.loc[(df.is_forecastable == True)]
+#df_coverage = df.loc[(df.is_forecastable == True) & (df.item_usage_rule == 'Use')]
+#df_adherence = df_coverage.loc[df_coverage.baseline == '2. Statistical']
+#df_not_adherence = df_coverage.loc[df_coverage.baseline == '1. Manual']
+
+
+#number_items = len(df)
+#number_forecastable = len(df_forecastable)
+#number_coverage = len(df_coverage)
+#adherence = len(df_adherence)
+
+
+
+#df_test_new_items = df_forecastable.loc[(df_forecastable.item_usage_rule == 'Ignore') & (df_forecastable.intermit < 0.3)]
--- zero-touch-analysis/main.py
+++ zero-touch-analysis/main.py
@@ -6,10 +6,24 @@ import os
 
 
 path = r'/Users/i0557167/Documents/Git/Statistical-Forecasting/zero-touch-analysis/data' # use your path
-all_files = glob.glob(os.path.join(path, "*.xlsx"))
+all_files = glob.glob(os.path.join(path, "*.csv"))
 
-df_monthly = pd.read_excel(path+"/monthlydata.xlsx")
-df_cumulative = pd.read_excel(path+"/cumulativedata.xlsx")
+df_forecastability = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
+
+df_forecastability.market_code.fillna('NA', inplace=True)
+
+
+
+df_forecastability['forecast_item'] = df_forecastability['forecast_item'].map(lambda x: str(x)[4:])
+
+df_forecastable = df_forecastability.loc[(df_forecastability.is_forecastable == True) & (df_forecastability.item_usage_rule == 'Use')]
+
+df_acov = df_forecastable.loc[:,['market_code','forecast_item','acov']]
+df_acov.rename(columns={'market_code': 'MARKET', 'forecast_item': 'GMID','acov':'ACOV'}, inplace=True)
+
+df_monthly = pd.read_excel(path+"/monthly_PH.xlsx")
+df_cumulative = pd.read_excel(path+"/cumulative_PH.xlsx")
+
 
 # change all column names to uppercase
 df_monthly.columns = df_monthly.columns.str.upper()
@@ -24,13 +38,25 @@ df_cumulative.columns = df_cumulative.columns.str.replace(" ", "_")
 df_monthly = df_monthly.replace('-', 0)
 df_cumulative = df_cumulative.replace('-', 0)
 
+#marina_market_codes = {"INDIA": 'IN', "BRAZIL": 'BR', "KOREA": 'KR', "HONG KONG": 'HK', "PHILIPPINES": 'PH'}
+marina_market_codes = { "BRAZIL": 'BR', "KOREA": 'KR', "HONG KONG": 'HK', "PHILIPPINES": 'PH'}
+#marina_market = {"INDIA": 0.40, "BRAZIL": 0.45, "KOREA": 0.31, "HONG KONG": 0.36, "PHILIPPINES": 0.33}
+marina_market = {"BRAZIL": 0.45, "KOREA": 0.31, "HONG KONG": 0.36, "PHILIPPINES": 0.33}
+nurdos_market = {"ALGERIA": 0.46, "EGYPT": 0.47, "MOROCCO": 0.35, "SOUTH AFRICA": 0.38, "TUNISIA": 0.35, "UNITED ARAB EMIRATES": 0.53, "SAUDI ARABIA": 0.53, "TURKEY": 0.53, "JAPAN": 0.19}
+romina_market = {"ARGENTINA": 0.32, "POLAND": 0.45, "ROMANIA": 0.45, "HUNGARY": 0.35, "SLOVAKIA": 0.50, "CZECH REPUBLIC": 0.50, "MOLDOVA": 0.55}
 
+
+final_df = pd.DataFrame()
+
+
+
+for key in marina_market: 
 # select country
-df_country = df_cumulative[(df_cumulative.MARKET == 'INDIA')]
-df_stat_mape_on_target = df_country[df_country.STAT_MAPE < 0.30].reset_index(drop=True)
+    df_country = df_cumulative[(df_cumulative.MARKET == key)]
+    df_stat_mape_YTD_on_target = df_country[df_country.STAT_MAPE_YTD < marina_market[key]]
 
 # select negative enrichments
-df_negative_enrichments = df_stat_mape_on_target[df_stat_mape_on_target.DELTA_MAPE_VARIATION < 0].sort_values(by="VOLUME", ascending=False).reset_index(drop=True)
+    df_negative_enrichments = df_stat_mape_YTD_on_target[df_stat_mape_YTD_on_target.DELTA_MAPE_VARIATION < 0].sort_values(by="VOLUME", ascending=False)
 
 
 #--------------------------- count of the enrichments -----------------------
@@ -38,10 +64,26 @@ df_negative_enrichments = df_stat_mape_on_target[df_stat_mape_on_target.DELTA_MA
 
 #list of gmids to analyse
 
-gmid_list = df_negative_enrichments.GMID.reset_index(drop=True)
+    gmid_list = df_negative_enrichments.GMID
+    
+    for gmid in gmid_list:
+        stage_df = df_monthly[df_monthly.GMID == gmid].sort_values(by=['STARTDATE']).loc[:,["MARKET","GMID","STARTDATE",'DELTA_MAPE_VARIATION','STAT_MAPE','FINAL_MAPE']]
+        final_df = pd.concat([final_df,stage_df])
 
-for gmid in gmid_list:
-    print(df_monthly[df_monthly.GMID == gmid].sort_values(by=['STARTDATE']).reset_index(drop= True)["DELTA_MAPE_VARIATION"])
+
+for key in marina_market_codes:
+    final_df.MARKET.replace(key,marina_market_codes[key], inplace=True)
+
+df_acov['GMID'] = df_acov['GMID'].astype('int64')
+final_df = pd.merge(final_df,df_acov)
+stat_MAPE_YTD_final = df_negative_enrichments.loc[:,["GMID","STAT_MAPE_YTD","FINAL_MAPE_YTD"]]
+final_df = pd.merge(final_df,stat_MAPE_YTD_final)
+
+#final_df.merge(df_acov, left_on = 'GMID', right_on = 'GMID')
+
+
+final_df.reset_index(drop=True).to_excel(path+"/marina_zerotouch_PH.xlsx",index=False)
+
 
 
 
