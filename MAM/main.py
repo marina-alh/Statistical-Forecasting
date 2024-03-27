2b69cfc55eefce8580cf34f4790ec01fe820f129
marina <marina.andrade@sanofi.com>
2024-03-27 11:48:59 +0100

    new read excel files function

--- MAM/main.py
+++ MAM/main.py
@@ -1,9 +1,11 @@
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import glob
 import os
-import source as sc
+import source_module as sc
+
 
 #data path
 path = r'/Users/i0557167/Library/CloudStorage/OneDrive-Sanofi/Documents/Market/MAM/2024/MAR24/data/' 
@@ -13,7 +15,7 @@ sanofi_colors = ["#23004c","#7A00E6","#62d488","#ed6c4e","#f6c243","#ca99f5"]
 sns.set_palette(sns.color_palette(sanofi_colors))
 
 
-monthly_kpi = pd.read_excel(path+'all_my_markets_YTD.xlsx') 
+monthly_kpi = sc.read_xlsx_files(path+"YTD")
 six_months_kpi = pd.read_excel(path+'all_my_markets_last_6months_not_acumulated.xlsx')
 my_scope = pd.read_excel(path+'Configuration_ALL.xlsx', skiprows=3)
 leg1leg3 = pd.read_excel(path+'LEG1LEG3.xlsx')
@@ -38,6 +40,7 @@ my_scope.drop(columns=columns_to_drop, axis=1, inplace=True)
 # Initial setup for my forecast scope
 
 marina_market_codes = {"INDIA": 'IN', "BRAZIL": 'BR', "KOREA": 'KR', "HONG KONG": 'HK', "PHILIPPINES": 'PH'}
+marina_market_target_mape = {"INDIA": 40, "BRAZIL": 45, "KOREA": 31, "HONG KONG": 36, "PHILIPPINES": 33}
 
 # OVERVIEW OF MY SCOPE. THE GOAL IS TO TRACK THE CHANGES OVER THE MONTHS; 
 #   PLOT DONUTS PLOTS WITH ITEMS UNDER MANUAL - STAT - USE AND IGNORE 
@@ -77,7 +80,12 @@ my_scope_simple = filtered_my_scope[columns_to_keep]
 #filtered_switch_dataset = switch_dataset[(switch_dataset['TYPE'] == 'Switch-In') & & (switch_dataset['OTHER_ITEMS'].isin(switch_dataset.loc[switch_dataset['USAGE_RULE'] == '2. Statistical', 'ITEM']))]
 #filtered_switch_dataset =switch_dataset[(switch_dataset['TYPE'] == 'Switch-In') & (switch_dataset['ACTIVE_BASELINE'] == '1. Manual') & (switch_dataset['OTHER_ITEMS'].isin(switch_dataset.loc[switch_dataset['ACTIVE_BASELINE'] == '2. Statistical', 'ITEM']))]
 
-
+""" Questions to be anwsered here under 2 and 3:
+        1 -  Are any of my configurations wrong? Stat under ignore? Manual under use? Why?
+        2 - Compare with last month's report to keep track of my changes
+        3 - Record the changes I do month to month
+        4 - Propose new forecastable items (QUARTELY)
+"""
 
 # 2/. FORECASTABILITY CHECK 
 # list of forecastable items but under ignore and manual
@@ -88,8 +96,6 @@ new_forecastable_items_list = new_forecastable['forecast_item'].tolist()
 configuration_new_forecastable = my_scope_simple[my_scope_simple['ITEM'].isin(new_forecastable_items_list)].reset_index(drop=True)
 # merging the info from config file with the acov and intermit columns by GMID key
 configuration_new_forecastable = pd.merge(configuration_new_forecastable,new_forecastable[['forecast_item','acov','intermit']],left_on="ITEM", right_on="forecast_item", how="outer").drop(columns=['forecast_item'])
-
-
 
 # 3/. OVERVIEW OF MY SCOPE
 # 3.1/. My active scope
@@ -104,19 +110,53 @@ my_active_scope.columns = my_active_scope.columns.str.upper()
 #number of items I have active
 number_active_scope = len(my_active_scope)
 
-# 3.2/. Check if I have manual items in use
+# 3.2/. Check if I have manual items in use. 
 my_manual_use = my_scope_simple[(my_scope_simple.ACTIVE_BASELINE =='1. Manual')&(my_scope_simple.TYPE != 'Prunned') & (my_scope_simple.USAGE_RULE == 'Use')].reset_index(drop=True)
+my_manual_use_items_list = my_manual_use['ITEM'].tolist()
+
+matching_rows_my_manual_use_forecastability = forecastability[forecastability['forecast_item'].isin(my_manual_use_items_list)].reset_index(drop=True)
+my_manual_use = pd.merge(my_manual_use,matching_rows_my_manual_use_forecastability[['forecast_item','acov','intermit','is_forecastable']],left_on="ITEM", right_on="forecast_item", how="outer").drop(columns=['forecast_item'])
 
 # 3.3/. Check if I have stat items in ignore
 my_statistical_ignore = my_scope_simple[(my_scope_simple.ACTIVE_BASELINE =='2. Statistical')&(my_scope_simple.TYPE != 'Prunned') & (my_scope_simple.USAGE_RULE == 'Ignore')].reset_index(drop=True)
 
 #4/. TOP 10 BEST AND TOP 10 OFFENDERS
+
+
+"""
+Questions to be answered here:
+        1 - Track top 10 performance by volume stat MAPE and final MAPE
+        2 - Plot last 6 months stat mape and final mape with the respective trend. 
+        3 - Quartely check all itemns with stat MAPE under target to propose new model. 
+        
+
+"""
         
 # 4.1/. TOP 10 OFFENDERS
 # filter for PHillipines
 
-filtered_country_monthly_kpi = monthly_kpi[monthly_kpi['MARKET'] == 'PHILIPPINES'].reset_index(drop=True)
 
+# --------- WARNING -------------- This column is already sorted from Sceye. Consider doing an extraction for each market to get the accurate Volume to each GMID for that market
+filtered_country_monthly_kpi = monthly_kpi[monthly_kpi['MARKET'] == 'PHILIPPINES'].reset_index(drop=True) 
+
+Ph_models_jan = [
+    774581,
+    774632,
+    774570,
+    774634,
+    774630,
+    774635,
+    774573,
+    774636,
+    774578,
+    774577,
+    774631,
+    774575
+]
+
+
+
+PH_adjustment_models = filtered_country_monthly_kpi[filtered_country_monthly_kpi['GMID'].isin(Ph_models_jan)].reset_index(drop=True)
 
 # Selecting KPI columns to be converted from Object to float
 columns_to_convert = filtered_country_monthly_kpi.columns[5:17]
@@ -124,25 +164,34 @@ columns_to_convert = filtered_country_monthly_kpi.columns[5:17]
 # Cleaning KPI columns from non numeric characters
 # Convert non-numeric values to NaN and then replace NaNs with zero
 
-filtered_country_monthly_kpi = sc.convert_columns_to_numeric(filtered_country_monthly_kpi, columns_to_convert)
+#filtered_country_monthly_kpi = sc.convert_columns_to_numeric(filtered_country_monthly_kpi, columns_to_convert)
 
 
 # Converting the KPI Columns to float
-filtered_country_monthly_kpi[columns_to_convert] = filtered_country_monthly_kpi[columns_to_convert].astype(float)
+#filtered_country_monthly_kpi[columns_to_convert] = filtered_country_monthly_kpi[columns_to_convert].astype(float)
+
 
 # selecting float columns to be multiplied by 100
-float_columns = filtered_country_monthly_kpi.select_dtypes(include='float').columns
-float_columns= float_columns.drop('Enrichment gain')# don't want multiply this one by 100
-filtered_country_monthly_kpi[float_columns] *= 100 
+#float_columns = filtered_country_monthly_kpi.select_dtypes(include='float').columns
+#float_columns= float_columns.drop('Enrichment gain')# don't want multiply this one by 100
+#filtered_country_monthly_kpi[float_columns] *= 100 
 
 # Sort by volume:
 
-sorted_df = filtered_country_monthly_kpi.sort_values(by='Volume', ascending=False).reset_index(drop=True)
+sorted_df = filtered_country_monthly_kpi.reset_index(drop=True)
+
+# here I can do sorted_df.head(10)
+
+my_top_10_volume = sorted_df.head(10)
+my_top_10_volume_gmid_list = my_top_10_volume['GMID'].astype(str).tolist() # need to convert to str because when doing .tolist to the int64 type it returns an empty list
 
 
+# Here I can filter for all GMID that are under target Stat MAPE YTD 
+"""
+algostudy_candidates = sorted_df.loc[sorted_df['Stat MAPE YTD'] > 33]
 
 
-# it would be good to have last month evolution of these top 10 items
+# Plot last 6 months stat mape and final mape with the respective trend. 
 
 filtered_country_6monthskpi = six_months_kpi[six_months_kpi['MARKET'] == 'PHILIPPINES']
 columns_to_convert2 = filtered_country_6monthskpi.columns[6:12]
@@ -153,6 +202,61 @@ filtered_country_6monthskpi[columns_to_convert2] = filtered_country_6monthskpi[c
 float_columns2 = filtered_country_6monthskpi.select_dtypes(include='float').columns
 
 filtered_country_6monthskpi[float_columns2] *= 100 
+
+last6_my_top_10 = filtered_country_6monthskpi[filtered_country_6monthskpi['GMID'].astype(str).isin(my_top_10_volume_gmid_list)].reset_index(drop=True)
+
+
+
+
+# Create line plot
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=last6_my_top_10, x=last6_my_top_10.index, y='Stat MAPE', label='Stat MAPE')
+sns.lineplot(data=last6_my_top_10, x=last6_my_top_10.index, y='Final MAPE', label='Final MAPE')
+
+
+# Add trend lines (optional)
+sns.regplot(data=last6_my_top_10, x=last6_my_top_10.index.ordinal, y='Stat MAPE', scatter=False, label='Stat MAPE trend')
+sns.regplot(data=last6_my_top_10, x=last6_my_top_10.index.ordinal, y='Final MAPE', scatter=False, label='Final MAPE trend')
+
+plt.xlabel('Month')
+plt.ylabel('MAPE Values in %')
+plt.title('Evolution of Stat MAPE and Final MAPE over the Past 6 Months')
+plt.legend()
+plt.show()
+
+"""
+"""
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Example DataFrame (replace with your actual data)
+data = {
+    'ID': ['A', 'B', 'B', 'A', 'C', 'C'],
+    'start_date': ['2023-01-01', '2023-02-01', '2023-03-01', '2023-01-01', '2023-02-01', '2023-03-01'],
+    'kpi1': [100, 120, 110, 80, 90, 85],
+    'kpi2': [200, 210, 205, 180, 190, 195]
+}
+
+df = pd.DataFrame(data)
+
+# Convert 'start_date' to datetime
+df['start_date'] = pd.to_datetime(df['start_date'])
+
+# Group by 'ID'
+groups = df.groupby('ID')
+
+# Create separate plots for each ID
+for ID, group in groups:
+    plt.figure(figsize=(8, 6))
+    sns.lineplot(data=group, x='start_date', y='kpi1', label='kpi1')
+    sns.lineplot(data=group, x='start_date', y='kpi2', label='kpi2')
+    plt.xlabel('Date')
+    plt.ylabel('KPI Values')
+    plt.title(f'Evolution of KPIs for ID {ID}')
+    plt.legend()
+    plt.show()
+"""
 
 #-----------------------------------------------------------------
 #5/. LEG 1 AND LEG3 EVAL FOR NEW CHANGED ALGOS
--- zero-touch-analysis/main.py
+++ zero-touch-analysis/main.py
@@ -8,21 +8,24 @@ import os
 path = r'/Users/i0557167/Library/CloudStorage/OneDrive-Sanofi/Documents/Market/MAM/2024/MAR24/zero-touch/data' # use your path
 all_files = glob.glob(os.path.join(path, "*.csv"))
 
-df_forecastability = pd.concat((pd.read_csv(f) for f in all_files), ignore_index=True)
+df_forecastability = pd.read_excel(path+"/forecastability_ALL.xlsx")
 
-df_forecastability.market_code.fillna('NA', inplace=True)
-
-
-
-df_forecastability['forecast_item'] = df_forecastability['forecast_item'].map(lambda x: str(x)[4:])
-
+# read only the numbers
+# df_forecastability['forecast_item'] = df_forecastability['forecast_item'].map(lambda x: str(x)[4:])
+# get only the forecastable and in use
 df_forecastable = df_forecastability.loc[(df_forecastability.is_forecastable == True) & (df_forecastability.item_usage_rule == 'Use')]
 
 df_acov = df_forecastable.loc[:,['market_code','forecast_item','acov']]
 df_acov.rename(columns={'market_code': 'MARKET', 'forecast_item': 'GMID','acov':'ACOV'}, inplace=True)
 
-df_monthly = pd.read_excel(path+"/monthly_PH.xlsx")
-df_cumulative = pd.read_excel(path+"/cumulative_PH.xlsx")
+# Extract numeric values (remove "PH:")
+df_acov['numeric_values'] = df_acov['GMID'].str.extract(r'(\d+)').astype(int)
+df_acov['GMID'] = df_acov['numeric_values']
+df_acov.drop(columns=['numeric_values'], inplace=True)
+
+
+df_monthly = pd.read_excel(path+"/monthly_ALL.xlsx")
+df_cumulative = pd.read_excel(path+"/cumulative_ALL.xlsx")
 
 
 # change all column names to uppercase
@@ -39,11 +42,11 @@ df_monthly = df_monthly.replace('-', 0)
 df_cumulative = df_cumulative.replace('-', 0)
 
 #marina_market_codes = {"INDIA": 'IN', "BRAZIL": 'BR', "KOREA": 'KR', "HONG KONG": 'HK', "PHILIPPINES": 'PH'}
-marina_market_codes = { "BRAZIL": 'BR', "KOREA": 'KR', "HONG KONG": 'HK', "PHILIPPINES": 'PH'}
+marina_market_codes = {"INDIA": 'IN'}
 #marina_market = {"INDIA": 0.40, "BRAZIL": 0.45, "KOREA": 0.31, "HONG KONG": 0.36, "PHILIPPINES": 0.33}
-marina_market = {"BRAZIL": 0.45, "KOREA": 0.31, "HONG KONG": 0.36, "PHILIPPINES": 0.33}
-nurdos_market = {"ALGERIA": 0.46, "EGYPT": 0.47, "MOROCCO": 0.35, "SOUTH AFRICA": 0.38, "TUNISIA": 0.35, "UNITED ARAB EMIRATES": 0.53, "SAUDI ARABIA": 0.53, "TURKEY": 0.53, "JAPAN": 0.19}
-romina_market = {"ARGENTINA": 0.32, "POLAND": 0.45, "ROMANIA": 0.45, "HUNGARY": 0.35, "SLOVAKIA": 0.50, "CZECH REPUBLIC": 0.50, "MOLDOVA": 0.55}
+marina_market = {"INDIA": 0.40}
+#nurdos_market = {"ALGERIA": 0.46, "EGYPT": 0.47, "MOROCCO": 0.35, "SOUTH AFRICA": 0.38, "TUNISIA": 0.35, "UNITED ARAB EMIRATES": 0.53, "SAUDI ARABIA": 0.53, "TURKEY": 0.53, "JAPAN": 0.19}
+#romina_market = {"ARGENTINA": 0.32, "POLAND": 0.45, "ROMANIA": 0.45, "HUNGARY": 0.35, "SLOVAKIA": 0.50, "CZECH REPUBLIC": 0.50, "MOLDOVA": 0.55}
 
 
 final_df = pd.DataFrame()
@@ -60,8 +63,6 @@ for key in marina_market:
 
 
 #--------------------------- count of the enrichments -----------------------
-
-
 #list of gmids to analyse
 
     gmid_list = df_negative_enrichments.GMID
@@ -74,30 +75,15 @@ for key in marina_market:
 for key in marina_market_codes:
     final_df.MARKET.replace(key,marina_market_codes[key], inplace=True)
 
-df_acov['GMID'] = df_acov['GMID'].astype('int64')
+
 final_df = pd.merge(final_df,df_acov)
 stat_MAPE_YTD_final = df_negative_enrichments.loc[:,["GMID","STAT_MAPE_YTD","FINAL_MAPE_YTD"]]
 final_df = pd.merge(final_df,stat_MAPE_YTD_final)
 
-#final_df.merge(df_acov, left_on = 'GMID', right_on = 'GMID')
-
-
-final_df.reset_index(drop=True).to_excel(path+"/marina_zerotouch_PH.xlsx",index=False)
+final_df.reset_index(drop=True).to_excel(path+"/marina_zerotouch_IN.xlsx",index=False)
 
 
 
 
-#top10 volumes
-#top10_GMID = df_country.nlargest(n=10, columns=['VOLUME']).GMID.reset_index(drop= True)
 
-
-# get all dates
-
-
-
-#df_monthly[df_monthly.GMID == 724619].sort_values(by=['STARTDATE']).reset_index(drop= True)
-
-
-
-#df_forecastable = df.loc[(df.is_forecastable == True) & (df.item_usage_rule == 'Use')]
 
